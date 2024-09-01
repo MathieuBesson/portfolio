ca3b32267f8062bc36457f92cd2b82e84730100c
Mathieu Besson <mathieubesson2001@gmail.com>
2024-09-01 20:02:05 +0200

    fix: Improve metadescription

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Mathieu Besson - Développeur Back-end à Rennes",
-  description: "Développeur Back-end, Développeur de vos solutions...",
+  description: "Développeur back-end spécialisé en Symfony / PHP basé à Rennes. Découvrez mon parcours et mes compétences en développement full-stack et administration système.",
   verification: { google: "8SLw0e_wvK8XB-JWvPLJVgwQgS4lbToxWUBaZ3aCXGg" },
   twitter: { card: "summary_large_image", site: "@site", creator: "@creator", "images": "https://mathieu-besson.fr/asset/image/og-image.jpg" },
   icons: {
@@ -21,3 +21,6 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+
