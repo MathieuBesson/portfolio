79a79954824b5cd21fafb4293ac46011d92452eb
Mathieu Besson <60424154+MathieuBesson@users.noreply.github.com>
2024-09-05 09:20:25 +0200

    fix: name to uppercase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Mathieu Besson - Développeur Back-end à Rennes",
+  title: "Mathieu BESSON - Développeur Back-end à Rennes",
   description: "Développeur back-end spécialisé en Symfony / PHP basé à Rennes. Découvrez mon parcours et mes compétences en développement full-stack et administration système.",
   verification: { google: "8SLw0e_wvK8XB-JWvPLJVgwQgS4lbToxWUBaZ3aCXGg" },
   twitter: { card: "summary_large_image", site: "@site", creator: "@creator", "images": "https://mathieu-besson.fr/asset/image/og-image.jpg" },
